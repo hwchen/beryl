f2bd3ad2312ffb2068478e9575b2193d233d123d
Jonathan Speiser <jspeis@gmail.com>
2019-05-01 20:14:54 -0400

    allows users to set secret in url params for convenience

--- src/middleware.rs
+++ src/middleware.rs
@@ -3,6 +3,8 @@ use actix_web::middleware::{Middleware, Started};
 use actix_web::error::{ErrorUnauthorized, ParseError};
 use actix_web::Result;
 
+pub const X_BERYL_SECRET: &str = "x-beryl-secret";
+
 pub struct VerifyApiKey {
     secret: String,
 }
@@ -17,13 +19,27 @@ impl VerifyApiKey {
 
 impl<S> Middleware<S> for VerifyApiKey {
     fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
-        let secret = req.headers()
-            .get("x-beryl-secret")
-            .ok_or(ErrorUnauthorized(ParseError::Header))?
-            .to_str()
-            .map_err(ErrorUnauthorized)?;
+        let secret_header = req.headers().get(X_BERYL_SECRET);
 
-        if secret == self.secret {
+        let qp_secret_is_valid = {
+            let qry = req.query();
+            let secret_query_param = qry.get(X_BERYL_SECRET);
+
+            match secret_query_param {
+                Some(val) => val == &self.secret,
+                _ => false
+            }
+        };
+
+        let header_secret_is_valid = match secret_header {
+            Some(result_val) => match result_val.to_str() {
+                    Ok(val) => val == self.secret,
+                    _ => false
+            },
+            _ => false
+        };
+
+        if qp_secret_is_valid || header_secret_is_valid {
             Ok(Started::Done)
         } else {
             Err(ErrorUnauthorized(ParseError::Header))
